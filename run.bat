58e6cfc6f4b3d1fa1a26005022562b74e448c54b
ShineYu <shine.yulingle@gmail.com>
2023-02-13 23:54:58 +0800

    fix: 修复本地部署脚本。

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 pip3 install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver --noreload
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
