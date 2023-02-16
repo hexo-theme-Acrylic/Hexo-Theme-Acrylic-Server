0732a2e531ddba3a5749b282e3c8c86d9167196f
ShineYu <shine.yulingle@gmail.com>
2023-02-17 03:54:57 +0800

    update: add log.

--- hexoweb/functions.py
+++ hexoweb/functions.py
@@ -313,6 +313,8 @@ def get_latest_version():
 
 
 def check_if_api_auth(request):
+    print("request token:", request.POST.get("token"))
+    print("get_setting() token:", get_setting("WEBHOOK_APIKEY"))
     if request.POST.get("token") == get_setting("WEBHOOK_APIKEY"):
         return True
     if request.GET.get("token") == get_setting("WEBHOOK_APIKEY"):
