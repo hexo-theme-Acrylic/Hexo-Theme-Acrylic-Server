fffd58a79189aac177729e47c0c67bcf2ead36d4
ShineYu <shine.yulingle@gmail.com>
2023-02-18 19:06:33 +0800

    feat: 添加取消订阅功能。

--- core/urls.py
+++ core/urls.py
@@ -96,6 +96,7 @@ urlpatterns = [
     path('pub/essay/', pub.get_essay, name='pub_essay'),
     path('pub/subscribe/', pub.subscribe, name='pub_subscribe'),
     path('pub/subscribe_sum/', pub.subscribe_sum, name='pub_subscribe_sum'),
+    path('pub/cancelSubscribe/', pub.cancelSubscribe, name='pub_cancelSubscribe'),
 
     re_path(r'^(?!api)^(?!pub).*$\.*', pages, name='pages'),
 ]
