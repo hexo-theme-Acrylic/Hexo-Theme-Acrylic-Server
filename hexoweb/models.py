50d9d318804bc3420d3d67db4fcff2fcbc796f9b
ShineYu <shine.yulingle@gmail.com>
2023-03-04 16:24:27 +0800

    update: 修改vercel脚本，修改字段配置

--- hexoweb/models.py
+++ hexoweb/models.py
@@ -75,7 +75,6 @@ class EssayModel(models.Model):
     values = models.TextField(max_length=0x7FFFFFFF, default="{}")
 
 class VerificationCodeModel(models.Model):
-    managed = True
     mail=models.CharField(primary_key=True, max_length=50, verbose_name='邮箱')
     name=models.CharField(unique=False, max_length=30, verbose_name='名称')
     key=models.CharField(unique=False, max_length=30, verbose_name='验证码')
